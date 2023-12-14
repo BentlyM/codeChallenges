ca4eeeb483a5eeb9ae24534744fa263602ebf9ca
BentlyM <metayerbently@gmail.com>
2023-12-14 12:59:14 -0500

    comments

--- Level 2/passGenerator.cpp
+++ Level 2/passGenerator.cpp
@@ -5,20 +5,23 @@
 
 using namespace std;
 
-const int MAX_LENGTH = 20;
+// Display
 const int GENERATE_PASSWORD = 1;
 const int DISPLAY_PASSWORDS = 2;
 const int EXIT = 3;
+// MAXIMUM constants
 const int MAX_PASSWORDS = 5;
+const int MAX_LENGTH = 20;
 
+// variable prototypes
 void displayPasswords(string, string);
 string genPassword(int);
 
 int main(){
-    int numPass = 0;
-    string password = "";
-    string passwords = "";
-    int choice;
+    int numPass = 0; // amount of passwords
+    string password = ""; // the given password
+    string passwords = ""; // all given passwords (all stored in one variable)
+    int choice; // ur choice
 
     do{
         cout << "\nChoose an option:\n";
@@ -26,20 +29,20 @@ int main(){
         cout << DISPLAY_PASSWORDS << ". Display Passwords\n";
         cout << EXIT << ". Exit Program\n";
         cout << "-->";
-        cin >> choice;
+        cin >> choice; // prompts choice
 
         switch(choice){
             case GENERATE_PASSWORD:
             if(numPass != MAX_PASSWORDS){
                 password = genPassword(password.length());
-                passwords += password + "\n";
+                passwords += password + "\n"; // adds password to passwords
                 ++numPass;
             }else{
                 cout << "\nYou have reached the maximum number of passwords.\n";
             }
                 break;
             case DISPLAY_PASSWORDS:
-                displayPasswords(password, passwords);
+                displayPasswords(password, passwords); // display passwords
                 break;
             case EXIT:
                 cout << "You have chosen to exit the program. \n";
