1f0b9b0691ff136b6f80a516e8f42ea491d9c1cb
BentlyM <metayerbently@gmail.com>
2023-12-20 21:47:54 -0500

    Closer...

--- Level 2/Matrix.cpp
+++ Level 2/Matrix.cpp
@@ -8,10 +8,14 @@ const int ROWS = 2;
 
 int main(){
     int MATRIX[ROWS][COLUMNS];
+    int i = 0;
+    int j = 0;
     cout << "Enter the elements of matrix: \n";
-    for(int i=0; i<ROWS; i++){
-        for(int j=0; j<COLUMNS; j++)
+
+    for(i=0; i<ROWS; i++){
+        for(j = 0; j<COLUMNS; j++)
         cin >> MATRIX[i][j];
         }
 
+    cout << MATRIX[0][0] * MATRIX[1][0] << endl; // reference
 }
