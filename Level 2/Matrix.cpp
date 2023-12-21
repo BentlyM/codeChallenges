dcb1a39df6eb9b59501080799a53c7ad880431c6
BentlyM <metayerbently@gmail.com>
2023-12-21 09:39:08 -0500

    Update code to calculate and print the square of each element in the first column of a 2x2 matrix. (generated by blackbox.ai )

--- Level 2/Matrix.cpp
+++ Level 2/Matrix.cpp
@@ -1,21 +1,42 @@
+// Headers
 #include <iostream>
 #include <iomanip>
 #include <string>
 using namespace std;
 
+// constants
 const int COLUMNS = 2;
 const int ROWS = 2;
+
+// Prototypes
+int multiMaxtrix(int[ROWS][COLUMNS]);
 
 int main(){
     int MATRIX[ROWS][COLUMNS];
     int i = 0;
     int j = 0;
+    int index = 0;
     cout << "Enter the elements of matrix: \n";
 
     for(i=0; i<ROWS; i++){
-        for(j = 0; j<COLUMNS; j++)
-        cin >> MATRIX[i][j];
+        cout << "Row " << index + 1 << ":" << endl;
+        index++;
+        for(j = 0; j<COLUMNS; j++){
+            cin >> MATRIX[i][j];
         }
+    }
 
-    cout << MATRIX[0][0] * MATRIX[1][0] << endl; // reference
+    multiMaxtrix(MATRIX);
+
 }
+
+int multiMaxtrix(int MATX[ROWS][COLUMNS]){
+    int i;
+    int j = 0;
+
+    for(i = 0; i < ROWS; i++){
+        cout << MATX[i][j] * MATX[i][j] << endl; // reference
+        ++j;
+    }
+}
+
