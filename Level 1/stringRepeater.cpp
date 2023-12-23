50567f2f69c30dc16bdf3b4fff36d6ffb2457a48
BentlyM <metayerbently@gmail.com>
2023-12-22 22:50:07 -0500

    Another Challange

---
+++ Level 1/stringRepeater.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+const int MAX_REPEATS = 10;
+
+
+int main(){
+    string word;
+    int repeats;
+    cout << "\n|String Repeater|" << endl;
+    cout << "-----------------" << endl;
+    // Get the input from user.
+    cout << "Enter a word: ";
+    cin >> word;
+    cout << "Enter the number of times to repeat: ";
+    cin >> repeats;
+
+}
