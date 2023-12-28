fc24a7ddbeb0976dd9c1531f531bd6134862c916
BentlyM <metayerbently@gmail.com>
2023-12-28 15:13:20 -0500

    Added ternary operator example to the tutorial.

--- Personal Examples/Tutorial.cpp
+++ Personal Examples/Tutorial.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 using namespace std;
 
+// ternary operator
 int main(){
-    
+    int x;
+    string result;
+    cout << "Enter a number: ";
+    cin >> x;
+    // using if else statement
+
+    result = (x % 2 == 0) ? "even" : "odd";
+
+    cout << result;
+    exit(EXIT_SUCCESS);
 }
