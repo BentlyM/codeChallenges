62faa45fbb029f98185acfb795ff840eecb98f90
BentlyM <metayerbently@gmail.com>
2023-12-31 09:31:51 -0500

    erase

--- Personal Examples/Tutorial.cpp
+++ Personal Examples/Tutorial.cpp
@@ -2,16 +2,5 @@
 #include <iostream>
 using namespace std;
 
-// ternary operator
-int main(){
-    int x;
-    string result;
-    cout << "Enter a number: ";
-    cin >> x;
-    // using if else statement
+// playing around with things
 
-    result = (x % 2 == 0) ? "even" : "odd";
-
-    cout << result;
-    exit(EXIT_SUCCESS);
-}
