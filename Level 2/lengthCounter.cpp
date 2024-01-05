1a8a0a7b6b8747ea3041e6b30ef8d6d141e5d7fe
BentlyM <metayerbently@gmail.com>
2024-01-05 11:56:46 -0500

    Length Counter -- bot gonna try myself promise ;D

--- Level 2/lengthCounter.cpp
+++ Level 2/lengthCounter.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <sstream>
 
-int main()
-{
+int main() {
+    std::string sentence;
+    
+    // Input sentence
+    std::cout << "Enter a sentence: ";
+    std::getline(std::cin, sentence);
 
-    int Arr[10];
+    // Use a stringstream to split the sentence into words
+    std::istringstream iss(sentence);
+    std::vector<int> wordLengths;
 
-    for(int i = 0; i < 10; i++){
-        cin >> Arr[i];
+    // Iterate through each word and store its length
+    std::string word;
+    while (iss >> word) {
+        wordLengths.push_back(word.length());
     }
 
+    // Output word lengths
+    std::cout << "Word lengths: [";
+    for (size_t i = 0; i < wordLengths.size(); ++i) {
+        std::cout << wordLengths[i];
+        if (i < wordLengths.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    return 0;
 }
