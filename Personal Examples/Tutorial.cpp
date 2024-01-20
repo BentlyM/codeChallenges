252fc9f7b16844c8c4ec660a31be179ee4534698
BentlyM <metayerbently@gmail.com>
2024-01-20 08:23:22 -0500

    Simple interest

--- Personal Examples/Tutorial.cpp
+++ Personal Examples/Tutorial.cpp
@@ -5,4 +5,27 @@ using namespace std;
 
 int main(){
     
+    cout << "\nSimple Interest Calculator..." << endl;
+
+    int principal;
+    cout << "\nEnter principal amount: ";
+    cin >> principal;
+
+    double interest;
+    cout << "Enter interest rate (in percentage): ";
+    cin >> interest;
+    interest /= 10; // converted to percentage here
+
+    int time;
+    cout << "Enter Time (in years): ";
+    cin >> time;
+
+    double simpInterest;
+    simpInterest = (principal * interest * time) /10 ; // 10 instead of 100 because it was converted
+
+    cout << "\n Simple interest: " << simpInterest << endl;
+    cout << "Interest Rate: " << interest << endl;
+
+
+
 }
