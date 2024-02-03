fe281e6c8dd5a68f82e1bc29bc5f64a22a2f17e5
BentlyM <metayerbently@gmail.com>
2024-02-03 00:33:43 -0500

    //

--- Personal Examples/practicelevel.cpp
+++ Personal Examples/practicelevel.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Even or Odd Checker...\n" << endl;
-
-    int number;
-    cout << "Enter a  number: ";
-    cin >> number;
-
-    if(number % 2 != 0){
-        cout << number << " is an odd number";
-    }else{
-        exit(EXIT_FAILURE);
-    }
-}
+///
