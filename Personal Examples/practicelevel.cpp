735ee996a8b266b3236a5845b1cf827d4551a4d3
BentlyM <metayerbently@gmail.com>
2024-02-02 20:17:17 -0500

    pratice basic arithmetic

--- Personal Examples/practicelevel.cpp
+++ Personal Examples/practicelevel.cpp
@@ -1,1 +1,17 @@
-///
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1,number2;
+    cout << "Enter the first number: ";
+    cin >> number1;
+
+    cout << "Enter the second number: ";
+    cin >> number2;
+
+    cout << "sum: " << number1+number2 << endl;
+    cout << "Difference: " << number1-number2 << endl;
+    cout << "Product: " << number1*number2 << endl;
+    cout << "Quotient: " << number1/number2 << endl;
+
+}
