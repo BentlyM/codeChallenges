8934ceac07ccbeb1e8927328b6064f285fc23d36
BentlyM <metayerbently@gmail.com>
2024-02-10 13:04:07 -0500

    ///

--- Personal Examples/practicelevel.cpp
+++ Personal Examples/practicelevel.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int bucky;
-    cout << "Enter a number: ";
-    cin >> bucky;
-
-    bucky = (1 > 3) ? 10  : printf("Hello World!") + 5; // ternary operator
-    
-    if(bucky == 1){
-        cout << "You entered 1." << endl;
-    }else{
-        cout << "You didn't enter 1. You entered " << bucky << " instead!" << endl;
-    }
-
-    return 0;
-}
+///
