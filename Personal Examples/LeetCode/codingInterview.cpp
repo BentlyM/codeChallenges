1d51f9f24f2bf9f6a66d2720c2b205f4ded6f2e3
BentlyM <metayerbently@gmail.com>
2024-03-24 08:43:54 -0400

    .

--- Personal Examples/LeetCode/codingInterview.cpp
+++ Personal Examples/LeetCode/codingInterview.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 using namespace std;
 
-vector<int> resize(vector<int>& Arr){
-    vector<int> ans = Arr;
+vector<int>* resize(vector<int>& Arr){
+    vector<int>* ans = new vector<int>();
 
     for(int j = 0; j < Arr.size(); j++){
-            ans.push_back(Arr[j]);
+            ans->push_back(Arr[j]);
+    }
+    for(int j = 0; j < Arr.size(); j++){
+            ans->push_back(Arr[j]); 
     }
     return ans;
 }
@@ -14,9 +17,11 @@ vector<int> resize(vector<int>& Arr){
 int main(){
     vector<int> myArr = {1,2,1};
     
-    vector<int> newArray = resize(myArr);
-    for(int increment = 0; increment < newArray.size(); increment++){
-        cout << newArray[increment] << " ";
+    vector<int>* newArray = resize(myArr);
+    for(int increment = 0; increment < newArray->size(); increment++){
+        cout << (*newArray)[increment] << " ";
     }
     cout << endl;
+
+    delete newArray;
 }
