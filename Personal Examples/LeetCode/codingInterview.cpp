cc7ec7d065ea5f52cad815ff6576359d0c0fddba
BentlyM <metayerbently@gmail.com>
2024-04-02 21:50:22 -0400

    .

--- Personal Examples/LeetCode/codingInterview.cpp
+++ Personal Examples/LeetCode/codingInterview.cpp
@@ -4,24 +4,15 @@
 #include <vector>
 using namespace std;
 
-const int number = 2;
-
-int main(){
-    int i = 0;
+int main() {
+    vector<string> Mysentence;
     string sentence;
-    fstream outfile("listScrape" , ios::app);
-    vector<string> mySentence;
-    cout << "Inputs(max value[" << number << "]): " << "\n";
-    while(i != number){
-        getline(cin , sentence);
-        mySentence.push_back(sentence);
-        sentence = " ";
-        i++;
+    fstream inFile("listScrape" , ios::in);
+    
+    while(inFile << sentence){
+        if(){
+            
+        }
+        Mysentence.push_back(sentence);
     }
-    cout << "Outputs: " << endl;
-    for(int j = 0; j < number; j++){
-        cout << j + 1 << ". " << mySentence[j] << "\n";
-    }
-
-    outfile.close();
 }
