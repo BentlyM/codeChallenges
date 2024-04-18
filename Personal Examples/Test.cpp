51793db7c95c578ae390d6a5a54a1971dc71e24b
BentlyM <metayerbently@gmail.com>
2024-04-17 22:28:28 -0400

    .

--- Personal Examples/Test.cpp
+++ Personal Examples/Test.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
-#include <ctime>
-#include <stdlib.h>
-#include <string>
 using namespace std;
 
-string FavFood(string name){
-    string foods = "";
-
-    cout << "Hi " << name << "please emter your 3 favorite foods by a space:";
-    getline(cin , foods);
-
-    return foods;
-}
-
-string FavNums(){
-
-}
-
-
 int main(){
-    string fullname = "", firstname = "", lastname = "";
-    string foods = "", newFood = "", play_again = "";
-    int newNum = 0 , nums = 0;
+    int bucky[5];
 
-    cout << "Welcome to my program!!" << "\n";
-    cout << "Enter your fullname";
-    getline(cin , fullname);
-    int spaceIndex = fullname.find(" ");
-    string firstName = fullname.substr(0, spaceIndex);
-    string lastName = fullname.substr(spaceIndex + 1);
+    int *bp0 = &bucky[0];
+    int *bp1 = &bucky[1];
+    int *bp2 = &bucky[2];
+    int *bp3 = &bucky[3];
+    int *bp4 = &bucky[4];
 
-    printf("Let's get started...");
-    newFood = FavFood(fullname);
+    cout  << "bp0 is at " << bp0 << endl;
+    cout  << "bp1 is at " << bp1 << endl;
+    cout  << "bp2 is at " << bp2 << endl;
+    cout  << "bp3 is at " << bp3 << endl;
+    cout  << "bp4 is at " << bp4 << endl;
 
+      
 }
