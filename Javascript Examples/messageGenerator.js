013039caf2a34119f8b49f4769d434efa8e1f5f6
BentlyM <metayerbently@gmail.com>
2024-05-15 19:58:33 -0400

    completed

--- Javascript Examples/messageGenerator.js
+++ Javascript Examples/messageGenerator.js
@@ -12,7 +12,7 @@ Plan: a random inspirational message generator
         ->  ex2 - "In times of darkness, hope shines brightest, illuminating even the darkest corners."
 */
 
-inspiration = [
+const inspiration = [
     {
         wordTopic :  ["Love","Hope","Courage","Faith", "Dreams","Wisdom","Strength","Happiness","Freedom","Growth"],
 
@@ -23,17 +23,32 @@ inspiration = [
     },
     {
         verbs : ["Inspires","Encourages","Empowers","Ignites","Transforms","Motivates","Strengthens","Guides","Enlightens","Uplifts"],
+
         verb(){
-            return verbs[Math.floor(Math.random() * this.verbs.length)];
-        }
+            return this.verbs[Math.floor(Math.random() * this.verbs.length)];
+        },
     },
     {
         wordFocus : ["Heart","Minds","Souls","Lives","Dreams","Spirits","Journeys","Paths","Destinies","Possibilities"],
+
         focus(){
-            return wordFocus[Math.floor(Math.random() * this.wordFocus.length)];
+            return this.wordFocus[Math.floor(Math.random() * this.wordFocus.length)];
         }
+    },
+    {
+        conditional : ["..., guiding us toward a brighter tomorrow.","..., reminding us of our inner strength.","..., showing us that anything is possible.","..., illuminating the path to success.","..., inspiring us to reach for the stars.","..., encouraging us to never give up.","..., leading us to new beginnings.","..., reminding us that every cloud has a silver lining.","..., teaching us to embrace change.","..., proving that miracles happen every day."],
+        condition(){
+            return this.conditional[Math.floor(Math.random() * this.conditional.length)];
+        },
     }
 ]
 
+const main = () => {
+    const sentence = ["In the face of adversity ","With each sunrise ","Through the storms of life ","Amidst the chaos ","In moments of doubt "];
 
-console.log(inspiration[0].subject());
+    template = sentence[Math.floor(Math.random() * sentence.length)] + `${inspiration[0].subject()} ${inspiration[1].verb()} ${inspiration[2].focus()}${inspiration[3].condition()}`;
+
+    return template;
+}
+
+console.log(main())
