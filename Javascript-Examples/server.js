b2710345b6bdfbe581f01c8eb5b1bd5aa5a77a61
BentlyM <metayerbently@gmail.com>
2024-09-11 22:44:22 -0400

    s

--- Javascript-Examples/server.js
+++ Javascript-Examples/server.js
@@ -1,15 +1,36 @@
 import http, { createServer } from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT;
 
-const PORT = 3000;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const server = http.createServer((request, response) => {
-  // response.setHeader('Content-Type','text/html')
-  // response.statusCode = 404;
+ 
+const server = createServer((request, response) => {
 
-  response.writeHead(500, {'Content-type': 'application/json'});
-  response.end(JSON.stringify({message: 'server error'}));
+  try{
+    if(request.method == 'GET'){
+      let filePath;
+      if(request.url == '/'){
+        filePath = path.join(__dirname , 'Javascript-Example', 'index.html');
+      }else if (request.url === '/about'){
+        response.writeHead(200 , {'Content-type': 'text/html'});
+        response.end('<h1>About</h1>');
+      }else{
+        response.writeHead(404 , {'Content-type': 'text/html'});
+        response.end('<h1>Not Found</h1>');
+      }
+    }else{
+      throw new Error('Method is not allowed')
+    }
+  }catch(e){
+    response.writeHead(500 , {'Content-type': 'text/pain'});
+    response.end(`${e} <-- this is the error`);
+  }
 });
 
 server.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
